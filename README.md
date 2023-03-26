b7ee4be588f9a3fcb263afefc16fc03e4a740f61
yi-ge <a@wyr.me>
2023-03-26 20:19:19 +0800

    feat: info, poweroff, reboot apis

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # mini-linux-control-api
+
 One of the simplest Rust Linux control APIs for intranet low-power IoT devices.
+
+This is a simple Rust-based HTTP server that listens on port 65535 and provides three APIs:
+
+- `/info`: Returns the network interface information of the current Linux device.
+- `/halt`: Executes the halt command to halt the system.
+- `/reboot`: Executes the reboot command to reboot the system.
+
+## Dependencies
+
+- Rust programming language
+- Cargo package manager
+- `hyper` library (version 1.0.0-rc.3, with "full" features)
+- `tokio` library (version 1, with "full" features)
+- `http_body_util` library (for handling HTTP body data)
+
+## Setup
+
+1. Ensure Rust and Cargo are installed on your system. If not, follow the [official installation guide](https://www.rust-lang.org/tools/install).
+
+2. Clone this repository to your local machine and navigate to the project directory:
+
+    ```sh
+    git clone https://github.com/your_username/rust_api_server.git
+    cd rust_api_server
+    ```
+
+3. Build the project:
+
+    ```sh
+    cargo build
+    ```
+
+4. Run the server:
+
+    ```sh
+    cargo run
+    ```
+
+The server should now be running and listening on port 65535.
+
+## Usage
+
+You can interact with the API server using any HTTP client, such as `curl`, a web browser, or a REST client like Postman.
+
+### Get network interface information
+
+```sh
+curl <http://localhost:65535/info>
+```
+
+### Halt the system
+
+```sh
+curl -X POST <http://localhost:65535/halt>
+```
+
+### Reboot the system
+
+```sh
+curl -X POST <http://localhost:65535/reboot>
+```
+
+**Note:** Executing the `/halt` and `/reboot` commands requires root privileges. You may need to run the server with root privileges to use these endpoints.
+
+**Note:** The `#![deny(warnings)]` attribute in the code will cause all warnings to be treated as errors. You can comment out or remove this line if you wish to ignore the warnings.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ src/main.rs
@@ -0,0 +1,77 @@
+#![feature(exclusive_range_pattern)]
+#![deny(warnings)]
+
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::process::Command;
+
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{body::Incoming as IncomingBody, Request, Response};
+use tokio::net::TcpListener;
+
+async fn handle_request(req: Request<IncomingBody>) -> Result<Response<Full<Bytes>>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&hyper::Method::GET, "/info") => get_network_info().await,
+        (&hyper::Method::POST, "/halt") => halt().await,
+        (&hyper::Method::POST, "/reboot") => reboot().await,
+        _ => Ok(not_found()),
+    }
+}
+
+async fn get_network_info() -> Result<Response<Full<Bytes>>, Infallible> {
+    let output = Command::new("ip")
+        .arg("addr")
+        .output()
+        .expect("Failed to execute ip addr command");
+
+    let body = String::from_utf8_lossy(&output.stdout).to_string();
+    Ok(Response::new(Full::new(Bytes::from(body))))
+}
+
+async fn halt() -> Result<Response<Full<Bytes>>, Infallible> {
+    Command::new("sudo")
+        .arg("halt")
+        .spawn()
+        .expect("Failed to execute halt command");
+
+    Ok(Response::new(Full::new(Bytes::from("System halt initiated."))))
+}
+
+async fn reboot() -> Result<Response<Full<Bytes>>, Infallible> {
+    Command::new("sudo")
+        .arg("reboot")
+        .spawn()
+        .expect("Failed to execute reboot command");
+
+    Ok(Response::new(Full::new(Bytes::from("System reboot initiated."))))
+}
+
+fn not_found() -> Response<Full<Bytes>> {
+    Response::builder()
+        .status(hyper::StatusCode::NOT_FOUND)
+        .body(Full::new(Bytes::from("Not Found")))
+        .unwrap()
+}
+
+#[tokio::main]
+pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr: SocketAddr = ([0, 0, 0, 0], 65535).into();
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+
+    loop {
+        let (stream, _) = listener.accept().await?;
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(stream, service_fn(handle_request))
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
